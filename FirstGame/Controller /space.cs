68ac9f45c672e74cff6849b8bfaa99e75dc7e535
talm8994 <tom.tom.pop13@gmail.com>
2016-05-10 13:34:33 -0600

    ow

---
+++ FirstGame/Controller /space.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGame
+{
+	public class game1
+	{
+		public game1 ()
+		{
+		}
+	}
+}
+
---
+++ FirstGame/Model/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FirstGame
+{
+	public class Player
+	{
+		private int score;
+		private bool activate;
+		private health;
+
+		// Animation representing the player
+		public Texture2D PlayerTexture;
+
+		// Position of the Player relative to the upper left side of the screen
+		public Vector2 Position;
+
+		// State of the player
+		public bool Active;
+
+		// Amount of hit points that player has
+		public int Health;
+
+		// Get the width of the player ship
+		public int Width
+		{
+			get { return PlayerTexture.Width; }
+		}
+
+		// Get the height of the player ship
+		public int Height
+		{
+			get { return PlayerTexture.Height; }
+		}
+
+		public int Score
+		{
+			get{ return score;}
+			set{ score = ValueType; }
+		}	
+
+		public void Initialize(Texture2D texture, Vector2 position)
+		{
+			PlayerTexture = texture; 
+
+			// Set the starting position of the player around the middle of the screen and to the back
+			Position = position;
+
+			// Set the player to be active
+			Active = true;
+
+			// Set the player health
+			Health = 100;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{ 
+			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		}
+
+		public void Update()
+		{
+		}
+		public Player ()
+		{
+			
+		}
+	}
+}
+
